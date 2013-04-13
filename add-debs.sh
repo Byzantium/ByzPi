31b9f78cec66c13f9dd8589e246a3d1372417fc4
haxwithaxe <me@haxwithaxe.net>
2013-04-13 05:29:55 +0000

    adding readme for updating the apt repo and a script to do almost all of it for us

---
+++ add-debs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+basedir=$(realpath `basedir $0`)
+codename="wheezy"
+
+CWD=`pwd`
+
+cd $basedir #enter apt repo
+reprepro includedeb $codename ../*.deb # add debs to repo
+git add -A # add new files and stage changes
+git commit -a -m"auto-commit:add-deb.sh adding packages see log for details" # commit changes
+
+cd $CWD
+echo don't foget to push your changes once you've made sure they are correct
